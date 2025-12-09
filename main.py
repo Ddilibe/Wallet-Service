c53dda4ae450e6b48f2be7fc40f9ca1c6ff989a3
Ddilibe <franklinfidelugwuowo@gmail.com>
2025-12-09 23:37:32 +0100

    chore: Added db setup

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+
 from app.database import create_db_and_tables
 from app.routers import auth, keys, wallet
 
@@ -9,3 +14,9 @@ app = FastAPI(title="Wallet Service")
 app.include_router(auth.auth)
 app.include_router(keys.keys)
 app.include_router(wallet.wallet)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    create_db_and_tables()
+    yield
